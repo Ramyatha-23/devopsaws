400725249c9dc989e6d5d4d71a17dee1a21b41d1
Ramyatha-23 <ramyathareddy23@gmail.com>
2024-11-07 13:18:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import boto3
 client = boto3.client('ec2')
 response = client.run_instances(
     ImageId='ami-06b21ccaeff8cd686',
-    InstanceType='t2.micro',
+    InstanceType='t2.medium',
     KeyName='feature',
     MaxCount=1,
     MinCount=2
